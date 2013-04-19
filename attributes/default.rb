1eec95bf5256d131e92f10ca936c0d479238b3bf
Florin Popa <flopaconsult@gmail.com>
2013-04-19 18:05:11 +0300

    changes from florin-wrk on Fri Apr 19 18:05:11 EEST 2013

--- attributes/default.rb
+++ attributes/default.rb
@@ -19,4 +19,4 @@
 
 default['authorization']['sudo']['groups'] = Array.new 
 default['authorization']['sudo']['users'] = Array.new
-default['authorization']['sudo']['passwordless'] = true
+default['authorization']['sudo']['passwordless'] = false
